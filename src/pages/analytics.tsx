c99d9eec64c75679e4bc2360cfb7e0da7d28542b
dannyroberts95 <danielrdhr@gmail.com>
2023-01-31 16:18:31 +0000

    dynamic import fix

--- src/pages/analytics.tsx
+++ src/pages/analytics.tsx
@@ -1,4 +1,7 @@
 import dynamic from 'next/dynamic';
+import classes from './analytics.module.css';
+// import PieChart from '@/components/PieChart';
+// import TimeLine from '@/components/TimeLine';
 
 type PropTypes = {
   data: Link[];
@@ -7,13 +10,21 @@ type PropTypes = {
   clearLocalData: () => void;
 };
 
+const TimeLine = dynamic(() => import(`@/components/TimeLine`), {
+  ssr: false,
+});
+const PieChart = dynamic(() => import(`@/components/PieChart`), {
+  ssr: false,
+});
+
 export default function Index({ data }: PropTypes) {
-  const TimeLine = dynamic(() => import(`@/components/TimeLine`), {
-    ssr: false,
-  });
-  const PieChart = dynamic(() => import(`@/components/PieChart`), {
-    ssr: false,
-  });
+  if (data.length === 0) {
+    return (
+      <div className={classes.emptyMessage}>
+        <p>Add some links above to view analytics</p>
+      </div>
+    );
+  }
 
   return (
     <div>
